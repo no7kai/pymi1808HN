b43750eda7bde89bd375b1d20b5ea715bbb756c3
Nguyen Cong Thanh <no7kai@gmail.com>
2018-12-11 16:30:01 +0700

    list of a github user repo

--- githubrepos.py
+++ githubrepos.py
@@ -2,8 +2,26 @@ import requests
 import json
 import sys
 
+base_url = 'https://api.github.com/'
+headers = {'Content-Type': 'application/json',
+           'User-Agent': 'no7kai',
+           'Accept': 'application/vnd.github.v3+json'}
+
+
+def get_repos(username):
+	api_url = '{}users/{}/repos'.format(base_url, username)
+	r = requests.get(api_url, headers=headers)
+	if r.status_code == 200:
+		return r.json()
+	else:
+		print(('[!] Error HTTP {}'.format(r.status_code)))
+		return None
+
 username = sys.argv[1]
-r = requests.get('https://api.github.com/users/{}/repos'.format(username))
-data = r.json()
-for repo in data:
-    print(repo['name'])
+data = get_repos(username)
+
+if data is not None:
+    for repo in data:
+        print(repo['name'])
+else:
+	print('No repo list found!')
